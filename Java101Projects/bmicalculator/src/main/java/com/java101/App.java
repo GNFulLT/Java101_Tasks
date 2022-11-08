596540b6628ea0b84e187f6f15d2bc414150af26
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2022-11-08 17:27:38 +0300

    .

--- Java101Projects/bmicalculator/src/main/java/com/java101/App.java
+++ Java101Projects/bmicalculator/src/main/java/com/java101/App.java
@@ -12,10 +12,10 @@ public class App
     {
         Scanner scn = new Scanner(System.in);
         System.out.println( "Lütfen boyunuzu (metre cinsinde) giriniz : " );
-        double boy = scn.nextDouble();
+        double height = scn.nextDouble();
         System.out.println( "Lütfen kilonuzu giriniz : " );
-        double kilo = scn.nextDouble();
-        System.out.println( String.format("Vücut Kitle İndeksiniz : %.3f",kilo/(boy*boy)) );
+        double weight = scn.nextDouble();
+        System.out.println( String.format("Vücut Kitle İndeksiniz : %.3f",height/(weight*weight)) );
 
     }
 }
